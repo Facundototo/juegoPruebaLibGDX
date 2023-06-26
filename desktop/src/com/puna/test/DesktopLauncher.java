be0952c5dbbbbedc4de75e717efce32098155407
Facundototo <facutoto666@gmail.com>
2023-06-26 20:11:24 -0300

    primer commit

---
+++ desktop/src/com/puna/test/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.puna.test;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import com.puna.test.test;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("mi-test");
+		new Lwjgl3Application(new test(), config);
+	}
+}
